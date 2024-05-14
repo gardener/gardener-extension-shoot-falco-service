0595c8fa3d21282e2313addb7c7fbbe493b56191
i573718 <linus.roepert@sap.com>
2024-05-14 19:42:47 +0000

    Initial skeleton of validator

--- pkg/admission/validator/shoot_validator.go
+++ pkg/admission/validator/shoot_validator.go
@@ -8,88 +8,88 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/gardener/gardener-extension-shoot-falco-service/pkg/apis/service"
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	"github.com/gardener/gardener/pkg/apis/core"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 )
 
 // NewShootValidator returns a new instance of a shoot validator.
 func NewShootValidator(mgr manager.Manager) extensionswebhook.Validator {
-	return &shoot{
+	return &validator{
 		decoder: serializer.NewCodecFactory(mgr.GetScheme()).UniversalDecoder(),
 	}
 }
 
-// shoot validates shoots
-type shoot struct {
+// validator validates shoots
+type validator struct {
 	decoder runtime.Decoder
 }
 
 // Validate implements extensionswebhook.Validator.Validate
-func (s *shoot) Validate(ctx context.Context, new, _ client.Object) error {
-	_, ok := new.(*core.Shoot)
+func (v *validator) Validate(ctx context.Context, new, _ client.Object) error {
+	shoot, ok := new.(*core.Shoot)
 	if !ok {
 		return fmt.Errorf("wrong object type %T", new)
 	}
+	return v.validateShoot(ctx, shoot)
+}
 
-	//	return s.validateShoot(ctx, shoot)
+func (v *validator) validateShoot(_ context.Context, shoot *core.Shoot) error {
+	// Need check here
+	if v.isDisabled(shoot) {
+		return nil
+	}
+	allErrs := field.ErrorList{}
+
+	if len(allErrs) != 0 {
+		return allErrs.ToAggregate()
+	}
+
+	if _, err := v.extractFalcoConfig(shoot); err != nil {
+		return err
+	}
 	return nil
 }
 
-/*
-func (s *shoot) validateShoot(_ context.Context, shoot *core.Shoot) error {
-	if s.isDisabled(shoot) {
-		return nil
-	}
-	dnsConfig, err := s.extractDNSConfig(shoot)
-	if err != nil {
-		return err
-	}
-
-	allErrs := field.ErrorList{}
-	if dnsConfig != nil {
-		allErrs = append(allErrs, validation.ValidateDNSConfig(dnsConfig, shoot.Spec.Resources)...)
-	}
-
-	return allErrs.ToAggregate()
-}
-
 // isDisabled returns true if extension is explicitly disabled.
-func (s *shoot) isDisabled(shoot *core.Shoot) bool {
-	ext := s.findExtension(shoot)
+func (v *validator) isDisabled(shoot *core.Shoot) bool {
+	ext := v.findExtension(shoot)
 	if ext == nil {
 		return false
 	}
+
 	if ext.Disabled != nil {
 		return *ext.Disabled
 	}
 	return false
 }
 
-// extractDNSConfig extracts DNSConfig from providerConfig.
-func (s *shoot) extractDNSConfig(shoot *core.Shoot) (*apisservice.DNSConfig, error) {
-	ext := s.findExtension(shoot)
-	if ext != nil && ext.ProviderConfig != nil {
-		dnsConfig := &apisservice.DNSConfig{}
-		if _, _, err := s.decoder.Decode(ext.ProviderConfig.Raw, nil, dnsConfig); err != nil {
-			return nil, fmt.Errorf("failed to decode %s provider config: %w", ext.Type, err)
-		}
-		return dnsConfig, nil
-	}
-
-	return nil, nil
-}
-
-// findExtension returns shoot-dns-service extension.
-func (s *shoot) findExtension(shoot *core.Shoot) *core.Extension {
+// findExtension returns shoot-falco-service extension.
+func (v *validator) findExtension(shoot *core.Shoot) *core.Extension {
+	extensionType := "shoot-falco-service"
 	for i, ext := range shoot.Spec.Extensions {
-		if ext.Type == service.ExtensionType {
+		if ext.Type == extensionType {
+			fmt.Println(string(shoot.Spec.Extensions[i].ProviderConfig.Raw))
 			return &shoot.Spec.Extensions[i]
 		}
 	}
 	return nil
 }
-*/
+
+func (v *validator) extractFalcoConfig(shoot *core.Shoot) (*service.FalcoServiceConfig, error) {
+	ext := v.findExtension(shoot)
+	if ext != nil && ext.ProviderConfig != nil {
+		// dnsConfig := &apisservice.DNSConfig{}
+		falcoConfig := &service.FalcoServiceConfig{}
+		if _, _, err := v.decoder.Decode(ext.ProviderConfig.Raw, nil, falcoConfig); err != nil {
+			return nil, fmt.Errorf("failed to decode %s provider config: %w", ext.Type, err)
+		}
+		return falcoConfig, nil
+	}
+	return nil, nil
+}
