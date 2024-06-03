c16fd6122dd1f24f4dccbfff2bda29c879f7ec01
i573718 <linus.roepert@sap.com>
2024-06-03 21:53:26 +0000

    Fixed remaining linter warnings

--- pkg/apis/service/validation/validation.go
+++ pkg/apis/service/validation/validation.go
@@ -4,4 +4,35 @@
 
 package validation
 
-// Wil host utility functions for validator
+import (
+	"strconv"
+
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
+	"github.com/gardener/gardener-extension-shoot-falco-service/pkg/apis/service"
+)
+
+func ValidateFalcoServiceConfig(config *service.FalcoServiceConfig) field.ErrorList {
+	allErrs := field.ErrorList{}
+	if !isSupportedFalcoServiceVersion(*config.FalcoVersion) {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("falcoVersion"), *config.FalcoVersion, "Falco version is not supported"))
+	}
+	if config.Resources == nil || (*config.Resources != "gardener" && *config.Resources != "falcoctl") {
+		allErrs = append(allErrs, field.Invalid(field.NewPath("resources"), "", `resources must be set to "gardener" or "falcoctl"`))
+		// no point to continue here
+		return allErrs
+	}
+	if *config.Resources == "gardener" {
+		ruleRefs := config.Gardener.RuleRefs
+		for i, rule := range ruleRefs {
+			if rule.Ref == "" {
+				allErrs = append(allErrs, field.Invalid(field.NewPath("gardener.ruleRefs["+strconv.Itoa(i)+"]"), "", "Rule reference is empty"))
+			}
+		}
+	}
+	return allErrs
+}
+
+func isSupportedFalcoServiceVersion(_ string) bool {
+	return true
+}
