0170dffbbee39aaa9dd1d02e4d46b587b07c18f2
Dirk Marwinski <dirk.marwinski@sap.com>
2024-11-28 12:20:47 +0000

    fix 4

--- test/integration/falco-integration.sh
+++ test/integration/falco-integration.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 3 ]; then
+  echo "usage: falco-integration.sh <garden kubeconfig> <project namespace> <shoot name>"
+  exit 1
+fi
+
+dir=$(dirname $0)
+cd $dir
+
 python3 -m venv .venv
 python3 -m pip install -r requirements.txt
 source .venv/bin/activate
