51918705b8cb7787037c4e8a359ec2b0e9c404d6
Dirk Marwinski <dirk.marwinski@sap.com>
2024-12-03 07:29:42 +0100

    some test refinements

--- test/integration/falco-integration.sh
+++ test/integration/falco-integration.sh
@@ -9,7 +9,7 @@ dir=$(dirname $0)
 cd $dir
 
 python3 -m venv .venv
-python3 -m pip install -r requirements.txt
 source .venv/bin/activate
+python3 -m pip install -r requirements.txt
 
-pytest test_falco.py  --garden-kubeconfig $1 --project-namespace $2 --shoot-name $3
+pytest test_falco.py --garden-kubeconfig $1 --project-namespace $2 --shoot-name $3
