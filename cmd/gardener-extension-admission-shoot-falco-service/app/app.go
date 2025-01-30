1b4f1a01d91962acbad0bca5a60e0652d815ad6e
i573718 <linus.roepert@sap.com>
2025-01-30 12:49:56 +0000

    Gardener api changes

--- cmd/gardener-extension-admission-shoot-falco-service/app/app.go
+++ cmd/gardener-extension-admission-shoot-falco-service/app/app.go
@@ -21,7 +21,7 @@ import (
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	componentbaseconfig "k8s.io/component-base/config"
+	componentbaseconfig "k8s.io/component-base/config/v1alpha1"
 	"k8s.io/component-base/version/verflag"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
