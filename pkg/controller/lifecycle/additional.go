b05cf0cfb73160f37b72ab1dfb0db866d4f72e29
Dirk Marwinski <dirk.marwinski@sap.com>
2026-06-12 12:07:09 +0000

    Fix namespace issue with additional components

--- pkg/controller/lifecycle/additional.go
+++ pkg/controller/lifecycle/additional.go
@@ -5,16 +5,21 @@
 package lifecycle
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 
 	"github.com/gardener/gardener/pkg/chartrenderer"
 	managedresources "github.com/gardener/gardener/pkg/utils/managedresources"
 	"github.com/gardener/gardener/pkg/utils/oci"
 	"github.com/go-logr/logr"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	sigsyaml "sigs.k8s.io/yaml"
 
 	"github.com/gardener/gardener-extension-shoot-falco-service/pkg/apis/config"
 )
@@ -52,7 +57,12 @@ func DeployAdditionalSeedResources(ctx context.Context, log logr.Logger, c clien
 			return fmt.Errorf("failed to render chart for %s: %w", res.Name, err)
 		}
 
-		data := map[string][]byte{"manifests.yaml": release.Manifest()}
+		manifests, err := injectNamespace(release.Manifest(), namespace)
+		if err != nil {
+			return fmt.Errorf("failed to inject namespace into manifests for %s: %w", res.Name, err)
+		}
+
+		data := map[string][]byte{"manifests.yaml": manifests}
 		keepObjects := false
 		forceOverwrite := false
 		if err := managedresources.Create(ctx, c, namespace, res.Name, nil, false, "seed", data, &keepObjects, nil, &forceOverwrite); err != nil {
@@ -63,4 +73,33 @@ func DeployAdditionalSeedResources(ctx context.Context, log logr.Logger, c clien
 	}
 
 	return nil
+}
+
+func injectNamespace(manifest []byte, namespace string) ([]byte, error) {
+	var out bytes.Buffer
+	decoder := yaml.NewYAMLOrJSONDecoder(bytes.NewReader(manifest), 4096)
+
+	for {
+		var obj unstructured.Unstructured
+		if err := decoder.Decode(&obj); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		if obj.Object == nil {
+			continue
+		}
+		if obj.GetNamespace() == "" {
+			obj.SetNamespace(namespace)
+		}
+		data, err := sigsyaml.Marshal(obj.Object)
+		if err != nil {
+			return nil, err
+		}
+		out.WriteString("---\n")
+		out.Write(data)
+	}
+
+	return out.Bytes(), nil
 }
